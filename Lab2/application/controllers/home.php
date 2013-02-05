0330687fbd449a04b5fe37e61bee73791b82b74f
David Woodruff <mustang2009cobra@gmail.com>
2013-02-05 12:56:44 -0700

    Started Lab2. Doing this one with CodeIgniter PHP framework. Added framework and initial homepage file (nothing useful yet)

---
+++ Lab2/application/controllers/home.php
@@ -0,0 +1,18 @@
+<?php
+
+class Home extends CI_Controller {
+
+	public function view($page = 'home'){
+		if(! file_exists('application/views/pages/'.$page.'.php')){
+			//No page for that
+			show_404();
+		}
+		
+		$data['title'] = ucfirst($page);
+		
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/'.$page, $data);
+		$this->load->view('templates/footer', $data);
+	}
+
+}
