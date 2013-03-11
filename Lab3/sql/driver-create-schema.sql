391d2efccc33f30add73874365999a81bcb04c94
David Woodruff <mustang2009cobra@gmail.com>
2013-03-11 17:30:53 -0600

    Merged sql into one file for each lab

--- Lab3/sql/driver-create-schema.sql
+++ Lab3/sql/driver-create-schema.sql
@@ -2,3 +2,19 @@ delimiter $$
 
 CREATE DATABASE `driver_site` /*!40100 DEFAULT CHARACTER SET latin1 */$$
 
+delimiter $$
+
+use `driver_site`$$
+
+delimiter $$
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(128) NOT NULL,
+  `lastName` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
